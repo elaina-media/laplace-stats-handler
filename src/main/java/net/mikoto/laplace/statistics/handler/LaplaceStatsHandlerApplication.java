057b2386e30bfcef04e969faee248ca4ac6bce92
mikoto <mikoto@ungine.cn>
2024-05-16 23:19:16 +0800

    :tada: 初次提交

---
+++ src/main/java/net/mikoto/laplace/statistics/handler/LaplaceStatsHandlerApplication.java
@@ -0,0 +1,13 @@
+package net.mikoto.laplace.statistics.handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaplaceStatsHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaplaceStatsHandlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/mikoto/laplace/statistics/handler/LaplaceStatsHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.laplace.statistics.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaplaceStatsHandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
